01d8d9335a0abb37f9f1dba4bfea66dfd2955de1
Ted Diño <ted23@tutanota.com>
2022-12-07 10:44:06 +0800

    Feat: upgrade to nextjs 13

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>
-          Netflix Philippines - Watch TV Shows Online, Watch Movies Online
-        </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{" "}
